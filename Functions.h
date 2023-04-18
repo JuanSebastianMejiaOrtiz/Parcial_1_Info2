bb4bea28ffda37a9edc83ab114885ca47fef9c16
Juan Sebastian Mejia Ortiz <juan.mejiao1@udea.edu.co>
2023-04-18 00:33:03 -0500

    Fix Error Branch Git, Intentar no volverlo a hacer

--- Functions.h
+++ Functions.h
@@ -9,7 +9,6 @@ using namespace std;
 void Programa();
 
 //Crear Matriz
-<<<<<<< HEAD
 unsigned char*** CreateMatrix(unsigned short filas, unsigned short columnas); //W
 unsigned char*** FillCursos(short* fil); //W
 
@@ -33,29 +32,5 @@ bool ExisteCursos(char* id); //Ez
 short ObtainCredito(unsigned char*** curso); //Ez
 int ContarCaracteres(unsigned char* cadena); //W
 unsigned char* JuntarChar(char* id, bool hoc); //hoc 0:curso, 1:horario
-=======
-unsigned char*** CreateMatrix(unsigned short filas, unsigned short columnas);
-void FillCursos(unsigned char*** cursos);
-
-//Modificar Horario
-void AddHTD(unsigned char*** horario);
-void Add_Act(unsigned char*** horario);
-void DeleteAct(unsigned char*** horario);
-
-//Mostrar en Terminal
-void PrintHorario(unsigned char*** horario);
-
-//Uso de Archivos
-void SaveHorario(unsigned char*** horario);
-void SaveCursos(unsigned char*** curso);
-unsigned char*** LoadHorario(char* id);
-unsigned char*** LoadCursos(char* id);
-bool ExisteHorario(char* id);
-bool ExisteCursos(char* id);
-
-//Obtener datos
-short ObtainCredito(unsigned char*** curso);
-int ContarCaracteres(char* cadena);
->>>>>>> e5ad702588acede14ff716883e7a50d333cf14ee
 
 #endif
