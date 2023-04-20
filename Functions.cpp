2e5b6498ab24abd687631d5b7b1e067e57b59dd7
Juan Sebastian Mejia Ortiz <juan.mejiao1@udea.edu.co>
2023-04-20 17:59:37 -0500

    No completo, version prealfa

--- Functions.cpp
+++ Functions.cpp
@@ -1,5 +1,51 @@
 #include "Functions.h"
 
+void Programa(){
+    unsigned char id[12] = "";
+    char menuop[12] ="";
+    unsigned char*** horario, curso;
+    short numcur = 0, i;
+    cout << "Bienvenido :}" << '\n';
+    cout << "Ingrese su identificacion : ";
+    cin >> id;
+
+    while(true){
+        cout << "Que desea hacer hoy? (use /help para ver los comandos disponibles.) : ";
+        cin >> menuop;
+
+        //Cargar Horario
+        if(menuop=="/hload"){
+            //horario = LoadHorario(id);
+            cout << "Esta funcion no esta disponible en esta version." << '\n';
+        }
+        else if(menuop=="/hsave_new"){
+            SaveHorario(horario, id);
+        }
+        else if(menuop=="/cload"){
+            //curso = FillCursosA(&numcur);
+            cout << "Esta funcion no esta disponible en esta version." << '\n';
+        }
+        else if(menuop=="/csave" && numcur!=0){
+            for (i = 0; i < numcur; i++){
+                SaveCursos(*(curso+i), numcur, id);
+            }
+        }
+        else if(menuop=="/ccreate"){
+            curso = FillCursos(&numcur);
+        }
+        else if(menuop=="/hadd"){
+            //Add_Act();
+            cout << "Esta funcion no esta disponible en esta version." << '\n';
+        }
+        else if(menuop=="/hdelete"){
+            DeleteAct(horario);
+        }
+        else if(menuop=="/help"){
+            cout << "/hload" << '\n';
+        }
+    }
+}
+
 //Crear Matriz
 unsigned char*** CreateMatrix(unsigned short filas, unsigned short columnas){
     //Le asigna espacio a la matriz
@@ -25,9 +71,11 @@ void AddHTD(){
 unsigned char*** FillCursos(short* fil){
     //Se va a llenar la matriz de cursos
     unsigned char*** cursos;
+    unsigned char** cur;
     short  caracteres = 65, filas, columnas = 7;
+    unsigned short i;
     bool lab = 0;
-    char cad[65] = "";
+    unsigned char cad[65] = "";
 
     cout << "Ingrese la cantidad de cursos que tiene matriculados : ";
     cin >> filas;
@@ -39,15 +87,17 @@ unsigned char*** FillCursos(short* fil){
         cout << "La materia que va a ingresar tiene laboratorio? (0/1 (No/Si)): ";
         cin >> lab;
         for (unsigned short c = 0; c < columnas; c++){
-            //Modificar
             if (c == 0){
                 cout << "Ingrese el codigo de la materia " << f+1 << " : ";
                 cin >> cad;
+                cur = LoadCurso(cad);
             }
-            //Quitar/Modificar
             else if (c == 1){
-                cout << "Ingrese el nombre de la materia " << f+1 << " (en vez de espacio ponga guión '-') (hasta 64 caracteres): ";
-                cin >> cad;
+                //Nombre Materia
+                for (i = 0; cur[0][i]!='\0'; i++){
+                    cad[i] = cur[0][i];
+                }
+                cad[i] = '\0';
             }
             else if (c == 2){
                 cout << "Ingrese los dias que le toca esa materia " << f+1 << " (sin contar laboratorios) (L:Lunes, M:Martes, W:Miercoles, J:Jueves, V:Viernes, S:Sabado, D:Domingo) (ejm: L-W-V): ";
@@ -71,34 +121,60 @@ unsigned char*** FillCursos(short* fil){
                 }
                 else cad[0] = '\0';
             }
-            //Quitar/Modificar
             else if (c == 6){
-                cout << "Ingrese la cantidad de creditos de la materia " << f+1 << " : ";
-                cin >> cad;
+                //Creditos
+                for (i = 0; cur[1][i]!='\0'; i++){
+                    cad[i] = cur[1][i];
+                }
+                cad[i] = '\0';
             }
-            for (unsigned short i = 0; cad[i]!='\0'; i++){
+
+            for (i = 0; cad[i]!='\0'; i++){
                 cursos[f][c][i] = cad[i];
             }
             cursos[f][c][caracteres] = '\0';
         }
+        delete cur;
     }
     return cursos;
 }
 
+unsigned char*** FillCursosA(short* fil){
+
+}
 
 //Modificar Horario
-void Add_Act(unsigned char*** horario);
+void Add_Act(unsigned char*** horario){
+
+}
 
 void DeleteAct(unsigned char*** horario){
-    char fi[5], co;
-    
-    cout << "Ingrese el dia de la actividad que desee eliminar (L:Lunes, M:Martes, W:Miercoles, J:Jueves, V:Viernes, S:Sabado, D:Domingo): ";
+    unsigned char fi[5], co;
+    short* horas;
+    short dias;
+
+    cout << "Ingrese el dia de la actividad que desee eliminar (L:Lunes, M:Martes, W:Miercoles, J:Jueves, V:Viernes, S:Sabado, D:Domingo)(NO L-V; SI L): ";
     cin >> co;
 
     cout << "Ingrese la hora que desea eliminar(ejm: 12-14): ";
     cin >> fi;
-
     
+    horas = SacarHoras(fi);
+
+    if (co=='L') dias = 0;
+    else if (co=='M') dias = 1;
+    else if (co=='W') dias = 2;
+    else if (co=='J') dias = 3;
+    else if (co=='V') dias = 4;
+    else if (co=='S') dias = 5;
+    else if (co=='D') dias = 6;
+
+    horas[0] -= 8;
+    horas[1] -= 9;
+
+    for (short i = 0; i < horas[1]; i++){
+        horario[horas[i]][dias][0] = '\0';
+    }
 }
 
 //Mostrar en Terminal
@@ -115,18 +191,165 @@ void PrintHorario(unsigned char*** horario){
 }
 
 //Uso de Archivos
-void SaveHorario(unsigned char*** horario);
-void SaveCursos(unsigned char*** cursos);
+void SaveHorario(unsigned char** horario, unsigned char id){
+    fstream archivo;
+    bool existe;
+    existe=ExisteHorario(id);
+    if (existe){
+        cout<<"El horario para este estudiante ya existe, desea sobre-escribirlo? (responda si o no)";
+        cin<<a;
+    }
+    if (a=="si"){
+        int NC_Id = ContarCaracteres(id), i;
+        unsigned char* Real = JuntarChar(id, 0);
+        char A[7+4+NC_Id+1+3] = "../";
+        fstream text;
+        bool flag;
+    
+        for (i = 0; i < NC_Id+7+4; i++){
+            A[i+3] = Real[i];
+            A[i+1+3] = '\0';
+        }
+        archivo.open(A, ios_base::out);
+        for (j=0;j<14;j++){
+            for (k=0;k<7;k++){
+                archivo << horario[i][j];
+                if (k!=6){
+                    archivo<<'\t';
+                }
+            }
+            archivo << '\n';     
+        }
+        archivo.close()      
+    }  
+}
+
 unsigned char*** LoadHorario(char* id);
 
-unsigned char** LoadCurso(char* code){
+void SaveCursos(unsigned char** cursos, short fil, unsigned char* id){
+    fstream archivo;
+    bool existe;
+    existe=ExisteCursos(id);
+    if (existe){
+        int NC_Id = ContarCaracteres(id), i, j, k;
+        unsigned char* Real = JuntarChar(id, 1);
+        char A[5+4+NC_Id+1+3] = "../";
+        for (i = 0; i < NC_Id+7+4; i++){
+            A[i+3] = Real[i];
+            A[i+1+3] = '\0';
+        }
+        archivo.open(A, ios_base::out);
+        for (j=0;j<fil;j++){
+            for (k=0;k<7;k++){
+                archivo<<cursos[i][j];
+                if (k!=6){
+                    archivo<<'\t';
+                }
+            }
+            archivo << '\n';
+        }
+        archivo.close();
+    }
+}
+
+unsigned char** LoadCursosA(int numeroLinea, unsigned char* code){
+    fstream archivo;
+    int contador=1,j,k,z,x,c,v,l,I;  
+    unsigned char** p = new unsigned char* [7];
+    // todo va a estar separado por tabulaciones codigo nombre creditos dias horas diaslaboratorio horaslaboratorio
+    bool flag;
+    int NC_Id = ContarCaracteres(code);
+    unsigned char* Real = JuntarChar(code, 1);
+    char A[7+4+NC_Id+1+3] = "../";
+
+    for (I = 0; I < NC_Id+7+4; I++){
+        A[I+3] = Real[I];
+        A[I+1+3] = '\0';
+    }
+    delete Real;
+    
+    archivo.open(A, ios_base::in);
+    flag=archivo.is_open();
+    if (flag){
+        const int longitud=66;
+        char* linea= new char [longitud];
+        while (archivo.getline(linea, longitud)){
+            if (contador == numeroLinea){
+               unsigned char* code=new unsigned char [11];
+                for (j=0;linea[j]!='\t';j++){
+                    code[j]=linea[j];
+                }
+                code[j+1]='\0';
+                p[0]=code;
+                unsigned char* nombre=new unsigned char [51];
+                for (k=0;linea[++j]!='\t';k++){
+                    nombre[k]=linea[j];
+                }
+                nombre[k+1]='\0';
+                p[1]=nombre;
+                unsigned char* cred=new unsigned char [3];
+                for (l=0;linea[++j]!='\t';l++){
+                    cred[l]=linea[j];
+                }
+                cred[l+1]='\0';
+                p[6]=cred;
+                unsigned char* dias=new unsigned char [14];
+                for (z=0;linea[++j]!='\t';z++){
+                    dias[z]=linea[j];
+                }
+                dias[z+1]='\0';
+                p[2]=dias;
+                unsigned char* horas=new unsigned char [6];
+                for (x=0;linea[++j]!='\t';x++){
+                    horas[x]=linea[j];
+                }
+                horas[x+1]='\0';
+                p[3]=horas;
+                unsigned char* diaslab=new unsigned char [3];
+                for (c=0;linea[++j]!='\t';c++){
+                    diaslab[c]=linea[j];
+                }
+                diaslab[c+1]='\0';
+                p[4]=diaslab;
+                unsigned char* horaslab=new unsigned char [6];
+                for (v=0;linea[++j]!='\t';v++){
+                    horaslab[v]=linea[j];
+                }
+                horaslab[v+1]='\0';
+                p[5]=horaslab;
+                archivo.close();
+                return p;
+            }
+            contador++;
+        }   
+        archivo.close();
+        p[0][0]='\0';
+        return p; 
+    }
+    else{
+        cout<<"ERROR abriendo el archivo"<<endl;
+        p[0][0]='\0';
+        return p;
+    }    
+} 
+
+unsigned char** LoadCurso(unsigned char* code){
+    //Carga la informacion de un solo curso
     fstream archivo;
     unsigned char** p = new unsigned char* [2];
-    bool flag, materia;
+    bool flag;
     int temp,j,i;
+    int NC_Id = ContarCaracteres(code);
+    unsigned char* Real = JuntarChar(code, 1);
+    char A[7+4+NC_Id+1+3] = "../";
+
+    for (i = 0; i < NC_Id+7+4; i++){
+        A[i+3] = Real[i];
+        A[i+1+3] = '\0';
+    }
+    delete Real;
     
-    //PONER UNA VARIABLE EN SU LUGAR (Parametro creo yo)
-    archivo.open ("Infocursos.txt", ios_base::in);
+    archivo.open (A, ios_base::in);
     flag = archivo.is_open();
     if (flag){
         const int longitud=66;
@@ -155,49 +378,11 @@ unsigned char** LoadCurso(char* code){
     }
     else{
         cout<<"Error en el archivo de cursos"<<endl;
+        p[0][0] = '\0';
+        p[1][0] = '\0';
     }
-} 
-
-/*
-unsigned char** LoadCurso(char* code){
-    fstream archivo;
-    unsigned char** p = new unsigned char* [2];
-    bool flag, materia;
-    int temp,j,i;
-    archivo.open("C:/Users/cuent/Documents/build-main-Desktop_Qt_6_5_0_MinGW_64_bit-Debug/Infocursos.txt", ios_base::in);
-    flag = archivo.is_open();
-    if (flag){
-        const int longitud=66;
-        char* linea= new char [longitud];
-        while (archivo.getline(linea, longitud)){
-            temp=0;
-            for (i=0;linea[i]!='\t';i++){
-                if (*(code+i)==linea[i]) temp++;
-            }
-            if (temp==i){
-                unsigned char* nombre=new unsigned char [50];
-                for (j=0;linea[++i]!='\t';j++){
-                    nombre[j]=linea[i];
-                }
-                nombre[j+1]='\0';
-                p[0]=nombre;
-                unsigned char* creditos=new unsigned char [2];
-                for (j=0;linea[++i]!='\n';j++){
-                    creditos[j]=linea[i];
-                }
-                p[1]=creditos;
-            }
-        }
-        archivo.close();
-        return p;
-    }
-    else{
-        cout<<"Error en el archivo de cursos"<<endl;
-    }
+    return p;
 }
-*/
-
-unsigned char*** LoadCursosM(char* id);
 
 bool ExisteHorario(unsigned char* id){
     int NC_Id = ContarCaracteres(id), i;
@@ -269,6 +454,7 @@ short Credito(unsigned char* cred){
 
 int ContarCaracteres(unsigned char* cadena){
     int i;
+    if (cadena[1] == '\0') return 1;
     for (i = 0; *(cadena+i) != '\0'; i++){}
     return i;
 }
@@ -316,4 +502,26 @@ unsigned char* JuntarChar(unsigned char* id, bool hoc){
         a[7+numcharID+3] = 't';
     }
     return a;
+}
+
+short* SacarHoras(unsigned char* chora){
+    short* array = new short[2];
+    unsigned char hora1[3], hora2[3];
+    int i, j;
+
+    for (i = 0; chora[i]!='-'; i++){
+        hora1[i] = chora[i];
+        hora1[i+1] = '\0';
+    }
+    i++;
+
+    for (j = 0;chora[i]!='\0'; i++, j++){
+        hora2[j] = chora[i];
+        hora2[j+1] = '\0';
+    }
+
+    array[0] = Credito(hora1);
+    array[1] = Credito(hora2);
+
+    return array;
 }
--- Functions.h
+++ Functions.h
@@ -9,25 +9,25 @@ using namespace std;
 void Programa();
 
 //Crear Matriz
-unsigned char*** CreateMatrix(unsigned short filas, unsigned short columnas); //Filas y columnas de la matriz que se va a crear
-unsigned char*** FillCursos(short* fil); //L
-unsigned char*** FillCursosA(); //L
+unsigned char*** CreateMatrix(unsigned short filas, unsigned short columnas);   //Filas y columnas de la matriz que se va a crear
+unsigned char*** FillCursos(short* fil);
+unsigned char*** FillCursosA(short* fil);
 
 //Modificar Horario
-void AddHTD(unsigned char*** horario, unsigned char *** cursos, short cfil); //L
-void Add_Act(unsigned char*** horario); //L
-void DeleteAct(unsigned char*** horario); //Ez
+void AddHTD(unsigned char*** horario, unsigned char *** cursos, short cfil);
+void Add_Act(unsigned char*** horario);
+void DeleteAct(unsigned char*** horario);
 
 //Mostrar en Terminal
-void PrintHorario(unsigned char*** horario); //Ez
+void PrintHorario(unsigned char*** horario);
 
 //Uso de Archivos
-void SaveHorario(unsigned char*** horario); //Lf
-void SaveCursos(unsigned char*** cursos, short fil); //Lf
+void SaveHorario(unsigned char*** horario, unsigned char* id);
+void SaveCursos(unsigned char*** cursos, short fil, unsigned char* id);
 
-unsigned char*** LoadHorario(unsigned char* id); //L
+unsigned char*** LoadHorario(unsigned char* id);
+unsigned char** LoadCursosA(int numeroLinea);
 unsigned char** LoadCurso(unsigned char* code);
-unsigned char*** LoadCursosA(unsigned char* id); //L
 
 bool ExisteHorario(unsigned char* id);
 bool ExisteCursos(unsigned char* id);
@@ -35,7 +35,8 @@ bool ExisteCursos(unsigned char* id);
 //Obtener datos
 short* ObtainHTI(unsigned char*** curso, short fil);
 short Credito(unsigned char* cred);
-int ContarCaracteres(unsigned char* cadena); //Cuenta los caracteres de una cadena
+int ContarCaracteres(unsigned char* cadena);    //Cuenta los caracteres de una cadena
 unsigned char* JuntarChar(unsigned char* id, bool hoc); //hoc 0:horario, 1:curso
+short* SacarHoras(unsigned char* chora);
 
 #endif
